95e3dc82b983cdff135875eb3ff269e559cb6b82
samuelwsz <samuelxwsz@gmail.com>
2023-09-12 13:39:17 -0300

    att

---
+++ .storybook/manager.ts
@@ -0,0 +1,7 @@
+import { addons } from "@storybook/addons"
+import { themes } from "@storybook/theming"
+
+//deixa o tema dark do storybook quando aberto no navegador
+addons.setConfig({
+  theme: themes.dark,
+})
